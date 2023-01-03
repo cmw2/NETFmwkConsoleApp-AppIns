41b61a51183a71565e7340bae46a7a8fe27e0699
Chris Westbrook <chwestbr@microsoft.com>
2023-01-03 10:47:30 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            // call weather api to get today's forecast
+            // call weather api to get tomorrow's forecast
+            var content = client.GetStringAsync("http://api.openweathermap.org/data/2.5/weather?q=Lancaster,PA&APPID=fe7e427fa3a74ac8fe2f7e47bb13c4c6").Result;
+            Console.WriteLine(content);
+            Console.ReadLine();
+        }
+    }
+}
